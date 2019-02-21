c83a28a8e920d8cfb6c7721cc75ee5ff725e988f
user <user@ME02>
2019-02-21 14:57:17 +0800

    second try

--- src/lab.java
+++ src/lab.java
@@ -3,7 +3,7 @@ public class lab {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		System.out.println("Hi,can you see that???");
 	}
 
 }
